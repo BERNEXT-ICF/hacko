d5448951e4f445624f24c3225376364efe97fb8e
Kelvin Dwipangga <144301420+klvnd@users.noreply.github.com>
2024-11-30 15:35:18 +0700

    stored in constant

---
+++ constants/course.ts
@@ -0,0 +1,38 @@
+export const courses = [
+    {
+      id: 1,
+      title: 'Web Development Bootcamp',
+      description: 'Learn full-stack web development with HTML, CSS, JavaScript, and React.',
+      image: 'https://media.istockphoto.com/id/469806772/vector/yellow-and-white-waves-seamless-pattern.jpg?s=612x612&w=0&k=20&c=f71etxOPJvaKEwCbGuPfNZi-WWGJafpYoTv2JE4eebQ=',
+      classCode: 'WD101',
+    },
+    {
+      id: 2,
+      title: 'Data Science with Python',
+      description: 'Master data analysis and machine learning using Python and popular libraries.',
+      image: 'https://media.istockphoto.com/id/469806772/vector/yellow-and-white-waves-seamless-pattern.jpg?s=612x612&w=0&k=20&c=f71etxOPJvaKEwCbGuPfNZi-WWGJafpYoTv2JE4eebQ=',
+      classCode: 'DS201',
+    },
+    {
+      id: 3,
+      title: 'UI/UX Design Essentials',
+      description: 'Become a pro at designing user-friendly interfaces and experiences.',
+      image: 'https://media.istockphoto.com/id/469806772/vector/yellow-and-white-waves-seamless-pattern.jpg?s=612x612&w=0&k=20&c=f71etxOPJvaKEwCbGuPfNZi-WWGJafpYoTv2JE4eebQ=',
+      classCode: 'UX301',
+    },
+    {
+      id: 4,
+      title: 'Mobile App Development',
+      description: 'Learn to build native apps for iOS and Android using React Native.',
+      image: 'https://media.istockphoto.com/id/469806772/vector/yellow-and-white-waves-seamless-pattern.jpg?s=612x612&w=0&k=20&c=f71etxOPJvaKEwCbGuPfNZi-WWGJafpYoTv2JE4eebQ=',
+      classCode: 'MA401',
+    },
+    {
+        id: 5,
+        title: 'Mobile App Development',
+        description: 'Learn to build native apps for iOS and Android using React Native.',
+        image: 'https://media.istockphoto.com/id/469806772/vector/yellow-and-white-waves-seamless-pattern.jpg?s=612x612&w=0&k=20&c=f71etxOPJvaKEwCbGuPfNZi-WWGJafpYoTv2JE4eebQ=',
+        classCode: 'MA401',
+      },
+  ];
+  
