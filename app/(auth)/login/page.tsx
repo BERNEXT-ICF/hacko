1967af9793fa44b3ccd6ddf00f0636137b2d64a3
mortomulu <mfajrihidayat09@gmail.com>
2024-12-04 04:07:13 +0700

    feat: integrate login & by google

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,25 +1,38 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import { AiFillEye, AiFillEyeInvisible, AiOutlineArrowLeft } from 'react-icons/ai';
+import React, { useState } from "react";
+import {
+  AiFillEye,
+  AiFillEyeInvisible,
+  AiOutlineArrowLeft,
+} from "react-icons/ai";
+import { useLogin } from "./api";
+import { useRouter } from "next/navigation";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [rememberMe, setRememberMe] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
 
+  const { mutate, isLoading, isSuccess } = useLogin();
+
+  const router = useRouter()
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    // Handle login logic here
-    console.log('Email:', email);
-    console.log('Password:', password);
-    console.log('Remember Me:', rememberMe);
+    mutate({ email, password, remember: rememberMe });
+    if (isSuccess) {
+      setEmail("");
+      setPassword("");
+      setRememberMe(false);
+    }
   };
 
   const handleGoogleLogin = () => {
-    // Handle Google login logic here
-    console.log('Login with Google');
+    router.push(
+      `${process.env.NEXT_PUBLIC_API_URL_DEV}/users/oauth/google/url`
+    );
   };
 
   const togglePasswordVisibility = () => {
@@ -32,10 +45,14 @@ const Login = () => {
         <form onSubmit={handleSubmit} className="flex flex-col">
           <p className="text-xs text-left flex items-center text-blue-500 mb-4">
             <AiOutlineArrowLeft className="mr-1" />
-            <a href="/" className="hover:underline">Back to Homepage</a>
+            <a href="/" className="hover:underline">
+              Back to Homepage
+            </a>
           </p>
           <h2 className="text-2xl text-center">Login to Hack[o]</h2>
-          <p className="text-gray-500 text-sm py-4">Welcome back! Please login to your account.</p>
+          <p className="text-gray-500 text-sm py-4">
+            Welcome back! Please login to your account.
+          </p>
           <label className="mb-2 text-sm">
             Email:
             <input
@@ -50,7 +67,7 @@ const Login = () => {
             Password:
             <div className="relative">
               <input
-                type={showPassword ? 'text' : 'password'}
+                type={showPassword ? "text" : "password"}
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
@@ -60,7 +77,7 @@ const Login = () => {
                 type="button"
                 onClick={togglePasswordVisibility}
                 className="absolute right-2 top-2 text-gray-500"
-                aria-label={showPassword ? 'Hide password' : 'Show password'}
+                aria-label={showPassword ? "Hide password" : "Show password"}
               >
                 {showPassword ? (
                   <AiFillEye size={24} />
@@ -80,22 +97,39 @@ const Login = () => {
               />
               Remember me
             </label>
-            <a href="#" className="text-sm text-blue-500 hover:underline">Forgot password?</a>
+            <a href="#" className="text-sm text-blue-500 hover:underline">
+              Forgot password?
+            </a>
           </div>
-          <button type="submit" className="p-2 text-base bg-yellow-500 text-white rounded">Login</button>
+          <button
+            type="submit"
+            className="p-2 text-base bg-yellow-500 text-white rounded"
+          >
+            Login
+          </button>
           <div className="flex items-center my-2">
             <hr className="flex-grow border-t border-gray-300" />
             <span className="mx-2 text-gray-500">or</span>
             <hr className="flex-grow border-t border-gray-300" />
           </div>
-          <button type="button" onClick={handleGoogleLogin} className="p-2 text-base bg-white text-gray-700 border border-gray-300 rounded flex items-center justify-center">
-            <img src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png" alt="Google icon" className="w-6 h-6 mr-2" />
+          <button
+            type="button"
+            onClick={handleGoogleLogin}
+            className="p-2 text-base bg-white text-gray-700 border border-gray-300 rounded flex items-center justify-center"
+          >
+            <img
+              src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png"
+              alt="Google icon"
+              className="w-6 h-6 mr-2"
+            />
             Login with Google
           </button>
           <p className="text-sm text-center mt-4">
-            Don't have an account? <a href="/signup" className="text-blue-500 hover:underline">SignUp</a>
+            Don't have an account?{" "}
+            <a href="/signup" className="text-blue-500 hover:underline">
+              SignUp
+            </a>
           </p>
-          
         </form>
       </div>
     </div>
